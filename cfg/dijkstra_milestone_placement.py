cb8d1a55466ece9aff1a59c143326d054730d029
Weifan <chenweifan9@gmail.com>
2022-12-04 12:43:59 -0500

    first working alg

--- cfg/dijkstra_milestone_placement.py
+++ cfg/dijkstra_milestone_placement.py
@@ -9,15 +9,8 @@ def place_milestone(g, benchmark, blackout_window = 10000, inplace=False):
     mg = dijkstra_solver(g, blackout_window = blackout_window)
     visualize(g, f'{benchmark}.tmsg.full.dot', label=['cost', 'iters'])
     visualize(g, f'{benchmark}.tmsg.dot', label='full')
-    binarize(mg, f'{benchmark}.tmsg')
+    #binarize(mg, f'{benchmark}.tmsg')
     return g
-
-def in_cycle(node, cycles):
-    """ cycles is the list of simple cycles, i.e. list(nx.alg.simple_cycles) """
-    for cycle in cycles:
-        if node in cycle:
-            return True
-    return False
 
 def calculate_min_weight(g, cycles, weight='cost'):
     min_self_cost = {}
@@ -33,32 +26,37 @@ def calculate_min_weight(g, cycles, weight='cost'):
     return min_self_cost
 
 def dijkstra_solver(g, blackout_window=10000):
-    nx.set_node_attributes(g, True, 'is_ms')  # equivalency of coloring all nodes red in paper
+    # set all nodes to red
+    nx.set_node_attributes(g, True, 'is_ms')  
+
+    # identify all cycles in the graph
     cycles = list(nx.algorithms.simple_cycles(g))
+
+    # calculate the cost for each loop. The cost is defined to be the following:
+    # pick any node in the loop, for that node, let PC travel from this node, until it loops back
+    # the total cost is the cost for that loop 
     self_costs = calculate_min_weight(g, cycles)
+
+    # traverse the graph
     entry = get_entry(g)
     red_nodes = list(nx.dfs_preorder_nodes(g, entry))
+
+    # if the cost of a loop is smaller than the blackout window
+    # color every node in the loop white
+    for n in red_nodes:
+        if n in self_costs and self_costs[n] <= blackout_window:
+            g.nodes[n]['is_ms'] = False
+    red_nodes = [n for n in red_nodes if g.nodes[n]['is_ms']]
+
+    # the following are by and large follow the paper
     while(red_nodes):
-        red_n = red_nodes[0]
-        if red_n in self_costs:
-            if self_costs[red_n] > blackout_window:
-                """TODO: the logic is slightly complicated. THink it over tmrw"""
-        else:
-            red_n = red_nodes.pop(0)
-            distances = nx.single_source_dijkstra_path_length(g, red_n, weight = 'cost')
-            for n in red_nodes:
-                g.nodes[n]['is_ms'] = False if distances[n] <= blackout_window else True
-            red_nodes = [node for node in red_nodes if distances[node] > blackout_window]
+        red_n = red_nodes.pop(0)
+        distances = nx.single_source_dijkstra_path_length(g, red_n, weight = 'cost')
+        for n in red_nodes:
+            if n in distances and distances[n] <= blackout_window:
+                g.nodes[n]['is_ms'] = False
+        red_nodes = [n for n in red_nodes if g.nodes[n]['is_ms']]
 
-    """TODO: one peculiar thing is that
-    A BB could loop back to itself eventually, 
-    thus it has a  well-defined shortest distance to itself.
-    the algorithm above would not consider this case, 
-    due to dijkstra considering this zero
-    thus need to patch it.
-    otherwise unsafe milestone would be placed. """
-
-    
 
 
 
